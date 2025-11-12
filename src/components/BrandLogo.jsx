c9fd61b43af1c210d0d2223dff7aba9a1f4a6ad9
syw1236 <syw1236@naver.com>
2025-11-12 19:53:28 +0900

    feat(BrandLogo): 브랜드 로고 컴포넌트 구현

---
+++ src/components/BrandLogo.jsx
@@ -0,0 +1,13 @@
+import "./BrandLogo.css";
+import logo from "../assets/logo.png";
+
+const BrandLogo = () => {
+  return (
+    <div className="BrandLogo">
+      <img src={logo} />
+      <p className="name">물막이판 관리 센터</p>
+    </div>
+  );
+};
+
+export default BrandLogo;
