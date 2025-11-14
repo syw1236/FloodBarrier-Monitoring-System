6b076a65152e9adf55d8713522c411449dd647d8
syw1236 <yewonseo183@gmail.com>
2025-11-14 12:22:26 +0900

    feat(WaterChart): 차트 데이터 추가 시 append로 추가 데이터만 렌더링 하도록 최적화

--- src/components/WaterChart.jsx
+++ src/components/WaterChart.jsx
@@ -1,6 +1,6 @@
 import * as echarts from "echarts";
 import "./WaterChart.css";
-import { useEffect, useRef, useContext } from "react";
+import React, { useEffect, useRef, useContext } from "react";
 import { MQTTContext } from "../context/MQTTContext";
 
 const WaterChart = () => {
@@ -9,6 +9,7 @@ const WaterChart = () => {
   const chartInstance = useRef(null);
   const sensors = ["Sensor 1", "Sensor 2", "Sensor 3"];
   const colors = ["#4B9CE2", "#9E77ED", "#F56C6C"];
+  const firstLoad = useRef(true);
 
   useEffect(() => {
     if (!chartRef.current) return;
@@ -53,20 +54,45 @@ const WaterChart = () => {
 
   useEffect(() => {
     if (!chartInstance.current) return;
-    chartInstance.current.setOption({
-      xAxis: {
-        type: "category",
-        data: (sensorData.topic1 || []).map((d) => d.time),
-      },
-      series: [
-        { data: sensorData.topic1.map((d) => d.value) },
-        { data: sensorData.topic2.map((d) => d.value) },
-        { data: sensorData.topic3.map((d) => d.value) },
-      ],
-    });
+
+    const chart = chartInstance.current;
+    const option = chart.getOption();
+
+    if (firstLoad.current) {
+      option.xAxis[0].data = sensorData.topic1.map((d) => d.time);
+      option.series[0].data = sensorData.topic1.map((d) => d.value);
+      option.series[1].data = sensorData.topic2.map((d) => d.value);
+      option.series[2].data = sensorData.topic3.map((d) => d.value);
+
+      chart.setOption(option);
+
+      firstLoad.current = false;
+      return;
+    }
+
+    const t1 = sensorData.topic1;
+    if (t1.length > 0) {
+      const latest = t1[t1.length - 1];
+      option.xAxis[0].data.push(latest.time);
+      option.series[0].data.push(latest.value);
+    }
+
+    const t2 = sensorData.topic2;
+    if (t2.length > 0) {
+      const latest = t2[t2.length - 1];
+      option.series[1].data.push(latest.value);
+    }
+
+    const t3 = sensorData.topic3;
+    if (t3.length > 0) {
+      const latest = t3[t3.length - 1];
+      option.series[2].data.push(latest.value);
+    }
+
+    chart.setOption(option);
   }, [sensorData]);
 
   return <div className="WaterChart" ref={chartRef}></div>;
 };
 
-export default WaterChart;
+export default React.memo(WaterChart);
